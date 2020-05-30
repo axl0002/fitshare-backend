dc0156e5b8c6bbee23e0a1c071a21d2b1aa614f4
Max Kovalovs <maxkovalovs@gmail.com>
2020-05-30 13:47:27 +0100

    Add a /users api endpoint

--- backend.py
+++ backend.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
 application = Flask(__name__)
+db_conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+
 
 @application.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@application.route('/users')
+def get_all_users():
+    cur = db_conn.cursor()
+    cur.execute("SELECT * FROM USERS;")
+    res = cur.fetchall()
+    cur.close()
+    return res
