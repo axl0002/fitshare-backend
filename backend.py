e340bdc27c9ed5920c2ae898aaaef6aef8d14ae7
George Mitenkov <georgemitenk0v@gmail.com>
2020-06-03 16:38:32 +0300

    added database upload to challenges on send

--- backend.py
+++ backend.py
@@ -2,6 +2,8 @@ from flask import Flask, request, Response
 import psycopg2
 import os
 from flask import jsonify
+from datetime import datetime
+import s3
 
 application = Flask(__name__)
 db_conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
@@ -35,4 +37,32 @@ def new_user():
     db_conn.commit()
     cur.close()
 
-    return Response(status=200)
+    return Response(status=201)
+
+
+@application.route('/send', methods=["POST"])
+def send():
+    json = request.get_json()
+    user_id = json["user_id"]
+    targets_ids = json["targets_ids"]
+    metadata = json["metadata"]
+    print(json)
+
+    # TODO: pass file and save it to storage
+
+    cur = db_conn.cursor()
+    cur.execute("SELECT friendsid FROM friends WHERE sourceid = %s AND targetid IN %s",
+                (user_id, tuple(targets_ids))
+                )
+    friends_ids = cur.fetchall()
+    print(friends_ids)
+    for friends_id in friends_ids:
+        dt = datetime.now()
+        cur.execute("INSERT INTO friend_challenges (metadata, friendsid, is_complete)"
+                    " VALUES (%s, %s, %s) RETURNING challengeid",
+                    (metadata, friends_id[0], False))
+        challenge_id = cur.fetchone()[0]
+        print(challenge_id)
+        db_conn.commit()
+    cur.close()
+    return Response(status=201)
