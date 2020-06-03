b10854637eaeeb5aaad92af1149837a0e965eda0
Max Kovalovs <maxkovalovs@gmail.com>
2020-06-03 23:05:47 +0100

    Remove old unneeded code

--- backend.py
+++ backend.py
@@ -9,15 +9,6 @@ application = Flask(__name__)
 db_conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
 
 
-@application.route('/users')
-def get_all_users():
-    cur = db_conn.cursor()
-    cur.execute("SELECT * FROM USERS;")
-    res = cur.fetchall()
-    cur.close()
-    return str(res)
-
-
 @application.route('/friends/<userid>')
 def get_friends(userid):
     cur = db_conn.cursor()
